bbb7762c1ec45cc13a9db480705947d7fcfabf75
ysicy <123982423+ysicy@users.noreply.github.com>
2023-10-23 23:06:20 +0300

    done..

--- src/test/java/qaguru/qa/TestBase.java
+++ src/test/java/qaguru/qa/TestBase.java
@@ -3,11 +3,14 @@ package qaguru.qa;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 
+import static com.codeborne.selenide.Selenide.open;
+
+
 public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
-        Configuration.baseUrl = "https://bigenc.ru/";
+        open("https://www.bigenc.ru/");
         Configuration.pageLoadStrategy = "eager";
         Configuration.browserSize = "1920x1080";
         Configuration.holdBrowserOpen = false;
--- src/test/java/qaguru/qa/TestWithJunit.java
+++ src/test/java/qaguru/qa/TestWithJunit.java
@@ -2,22 +2,25 @@ package qaguru.qa;
 
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$x;
 import static com.codeborne.selenide.Selenide.open;
 
 public class TestWithJunit extends  TestBase {
-    @BeforeEach
-    void setUp(){
-        open("https://www.bigenc.ru/");
-    }
 
-    @ValueSource(strings = {"Пушкин","Пушкин (город)"})
+
+    @ValueSource(strings = {"Пушкин", "Пушкин (город)"})
     @ParameterizedTest(name = "На странице bigenc.ru в футере нажимается кнопка 'поиск', отправляется неполный запрос и на странице результатов поиска проверяется значение")
-    @Tags({@Tag("Web"),@Tag("Search"),@Tag("UI")})
-    void successOpenSearchPage(String searchQuery){
+    @Tags({@Tag("Web"), @Tag("Search"), @Tag("UI")})
+    void successOpenSearchPageTest(String searchQuery) {
 
         $x("//div[contains(@class,'bre-header-nav-item _flex-start')][3]")
                 .click();
@@ -26,15 +29,21 @@ public class TestWithJunit extends  TestBase {
         $x("//a[contains(@class,'tw-grow')]").shouldHave(text(searchQuery));
     }
 
-
-    @DisplayName("На главной странице bigenc.ru в футере происходит переход на статическую страницу 'О проекте',на которой ищется ссылка на статью, происходит переход на статью и ставится лайк")
+    static Stream<Arguments> buttonsTest(){
+        return Stream.of(
+                Arguments.of(List.of("bre_quotes","bre_likes","bre-share", "bre-views"))
+        );
+}
+    @DisplayName("На главной странице bigenc.ru в футере происходит переход на статическую страницу 'О проекте',на которой ищется ссылка на статью, происходит переход на статью и в меню происходят действия")
     @Tags({@Tag("UI"),@Tag("Search")})
+    @CsvFileSource(emptyValue = "buttons.csv")
+    @MethodSource
     @Test
-    void successOpenStaticPageAndClickToButtons(){
+    void OpenStaticPageAndClickToButtonsTest(){
 
-        $x("//li[contains(@class,'_button bre-inline-menu__item')][1]").click();
+        $x("//a[contains(@href,'/p/about-project')]").click();
         $x("//a[contains(@href,'/c/sadovnichii-viktor-antonovich-edcd4f')][1]").click();
-        $x("//div[contains(@data-bre,'bre-likes')]").click();
+        $x("//div[contains(@class,'bre-article-page__content bre-article-content')]").click();
     }
 }
 
