bfad0d99185378e893cb5599c481bd77da85db16
a.shabelnikov <alexeyecko@gmail.com>
2023-10-24 16:38:45 +0300

    dooone!!!!!!!!

--- src/test/java/qaguru/qa/TestWithJunit.java
+++ src/test/java/qaguru/qa/TestWithJunit.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.*;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -29,15 +30,30 @@ public class TestWithJunit extends  TestBase {
         $x("//a[contains(@class,'tw-grow')]").shouldHave(text(searchQuery));
     }
 
-    static Stream<Arguments> buttonsTest(){
-        return Stream.of(
-                Arguments.of(List.of("bre_quotes","bre_likes","bre-share", "bre-views"))
-        );
-}
+    public Stream test(){
+        List<String> list = Arrays.asList("Пушкин", "Пушкин (город)");
+
+        return list.stream();
+    }
+
+    @MethodSource("test")
+    @ParameterizedTest(name = "На странице bigenc.ru в футере нажимается кнопка 'поиск', отправляется неполный запрос и на странице результатов поиска проверяется значение")
+    @Tags({@Tag("Web"), @Tag("Search"), @Tag("UI")})
+    void secondSuccessOpenSearchPageTest(String searchQuery) {
+
+        $x("//div[contains(@class,'bre-header-nav-item _flex-start')][3]")
+                .click();
+        $x("//input[contains(@type,'text')]").setValue(searchQuery).pressEnter();
+        System.out.println(searchQuery);
+        $x("//a[contains(@class,'tw-grow')]").shouldHave(text(searchQuery));
+    }
+
+
+
     @ParameterizedTest(name = "На главной странице bigenc.ru в футере происходит переход на статическую страницу 'О проекте',на которой ищется ссылка на статью, происходит переход на статью и в нем проверяется наличие кнопок")
     @Tags({@Tag("UI"),@Tag("Buttons"),@Tag("Search")})
     @CsvSource({"bre-quotes" , "bre-likes", "bre-share"})
-    void OpenStaticPageAndClickToButtonsTest(String buttonsQuery){
+    void openStaticPageAndClickToButtonsTest(String buttonsQuery){
 
         String type = String.format("//div[@data-bre = '%s']/span", buttonsQuery);
 
@@ -46,11 +62,11 @@ public class TestWithJunit extends  TestBase {
         $x("//a[contains(@href,'/c/sadovnichii-viktor-antonovich-edcd4f')][1]").click();
         $x("//div[contains(@class,'bre-article-page__content bre-article-content')]").shouldBe(visible);
         String attr = types.getCssValue("margin-top");
-
-        System.out.println(attr);
-
         Assertions.assertEquals("0px", attr);
     }
+
+
+
 }
 
 
