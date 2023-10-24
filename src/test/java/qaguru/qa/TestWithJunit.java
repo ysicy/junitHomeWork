4852f32a2f100d9a42430fbb74f2d86ef6bac28d
a.shabelnikov <alexeyecko@gmail.com>
2023-10-24 11:46:32 +0300

    dooone

--- src/test/java/qaguru/qa/TestWithJunit.java
+++ src/test/java/qaguru/qa/TestWithJunit.java
@@ -1,5 +1,7 @@
 package qaguru.qa;
 
+import com.codeborne.selenide.SelenideElement;
+import dev.failsafe.internal.util.Assert;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.*;
@@ -8,14 +10,13 @@ import java.util.List;
 import java.util.stream.Stream;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$x;
 
 
 public class TestWithJunit extends  TestBase {
 
-    @CsvSource (value = (
-        "Пушкин, Пушкин (город)"
-        ))
+
     @ValueSource(strings = {"Пушкин", "Пушкин (город)"})
     @ParameterizedTest(name = "На странице bigenc.ru в футере нажимается кнопка 'поиск', отправляется неполный запрос и на странице результатов поиска проверяется значение")
     @Tags({@Tag("Web"), @Tag("Search"), @Tag("UI")})
@@ -33,16 +34,22 @@ public class TestWithJunit extends  TestBase {
                 Arguments.of(List.of("bre_quotes","bre_likes","bre-share", "bre-views"))
         );
 }
-    @MethodSource("buttonsTest")
-    @ParameterizedTest(name = "На главной странице bigenc.ru в футере происходит переход на статическую страницу 'О проекте',на которой ищется ссылка на статью, происходит переход на статью и в меню происходят действия")
+    @ParameterizedTest(name = "На главной странице bigenc.ru в футере происходит переход на статическую страницу 'О проекте',на которой ищется ссылка на статью, происходит переход на статью и в нем проверяется наличие кнопок")
     @Tags({@Tag("UI"),@Tag("Buttons"),@Tag("Search")})
-    @CsvFileSource(emptyValue = "buttons.csv")
-    @Test
-    void OpenStaticPageAndClickToButtonsTest(){
+    @CsvSource({"bre-quotes" , "bre-likes", "bre-share"})
+    void OpenStaticPageAndClickToButtonsTest(String buttonsQuery){
 
+        String type = String.format("//div[@data-bre = '%s']/span", buttonsQuery);
+
+        SelenideElement types = $x(type);
         $x("//a[contains(@href,'/p/about-project')]").click();
         $x("//a[contains(@href,'/c/sadovnichii-viktor-antonovich-edcd4f')][1]").click();
-        $x("//div[contains(@class,'bre-article-page__content bre-article-content')]").click();
+        $x("//div[contains(@class,'bre-article-page__content bre-article-content')]").shouldBe(visible);
+        String attr = types.getCssValue("margin-top");
+
+        System.out.println(attr);
+
+        Assertions.assertEquals("0px", attr);
     }
 }
 
